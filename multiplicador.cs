a82afc06e38407dfe62ce817954077eb2b6e7e0d
ErickLopez0826 <xerick_cid@outlook.com>
2025-01-21 22:32:16 -0600

    Multiplicador

---
+++ multiplicador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class multiplicador
+{
+    static void Main(){
+        // El usuario ingresa el numero
+        Console.Write("Ingrese un número entero: ");
+        int numero;
+        // Ciclo while para determinar si el numero es entero o no
+        while (!int.TryParse(Console.ReadLine(), out numero))
+        {
+            Console.Write("Numero no valido, ingrese un numero entero: ");
+        }
+        int resultado = numero * 2;
+        Console.WriteLine($"El resultado de multiplicar {numero} por 2 es: {resultado}");
+    }
+}
